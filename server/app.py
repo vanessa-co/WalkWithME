1ca9a4443c9119d362887b71549da03725bc5e5d
vanessa coelho <vanessa.co403@gmail.com>
2023-04-27 11:29:34 -0400

    backend changes

--- server/app.py
+++ server/app.py
@@ -1,13 +1,14 @@
+
+
 import secrets
 from flask import request, jsonify
 from flask_restful import Resource
-from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
-from models import db, User, Walk, Review, Follow
-from config import app, api
+from models import User, Walk, Review, Follow
+from config import app, api, db
+import requests
+import bcrypt
 
-# JWT configuration
-app.config['JWT_SECRET_KEY'] = secrets.token_hex(16)  # Generate a 256-bit (32-byte) secret key
-jwt = JWTManager(app)
+app.config["SECRET_KEY"] = secrets.token_hex(16)
 
 class Home(Resource):
     def get(self):
@@ -15,120 +16,142 @@ class Home(Resource):
        
 api.add_resource(Home, '/')
 
-def user_exists(username, email):
-    return User.query.filter((User.username == username) | (User.email == email)).first() is not None
+class UserResource(Resource):
+    def get(self, user_id=None):
+        if user_id:
+            user = User.query.get(user_id)
+            return user.to_dict() if user else {"error": "User not found"}, 404
+        else:
+            users = User.query.all()
+            return [user.to_dict() for user in users]
 
-class UserRegistration(Resource):
+
     def post(self):
-        data = request.get_json()
-        if user_exists(data['username'], data['email']):
-            return {"message": "User already exists"}, 400
+        username = request.json.get('username')
+        email = request.json.get('email')
+        password = request.json.get('password')
 
-        new_user = User(
-            username=data['username'],
-            email=data['email'],
-            password=data['password']  # You should hash the password before saving it
-        )
-        db.session.add(new_user)
+        if not username or not email or not password:
+            return {"error": "All fields are required"}, 400
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            return {"error": "Username already exists"}, 409
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            return {"error": "Email already exists"}, 409
+
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        user = User(username=username, email=email, password_hash=hashed_password)
+        db.session.add(user)
         db.session.commit()
+
         return {"message": "User created successfully"}, 201
 
-class UserLogin(Resource):
+
+    def delete(self, user_id):
+        user = User.query.get(user_id)
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            return {"message": "User deleted successfully"}
+        return {"error": "User not found"}, 404
+
+api.add_resource(UserResource, '/users', '/users/<int:user_id>')
+
+class WalkResource(Resource):
+    def get(self, walk_id=None):
+        if walk_id:
+            walk = Walk.query.get(walk_id)
+            return walk.to_dict() if walk else {"error": "Walk not found"}, 404
+        walks = Walk.query.all()
+        return [walk.to_dict() for walk in walks]
+
     def post(self):
-        data = request.get_json()
-        user = User.query.filter_by(username=data['username']).first()
-        if user and user.password == data['password']:  # You should verify the hashed password
-            access_token = create_access_token(identity=user.id)
-            return {"access_token": access_token}, 200
-        else:
-            return {"message": "Invalid credentials"}, 401
+        name = request.json.get('name')
+        location = request.json.get('location')
+        description = request.json.get('description')
+        user_id = request.json.get('user_id')
 
+        if not name or not location or not user_id:
+            return {"error": "Name, location, and user_id are required fields"}, 400
 
-class UserWalks(Resource):
-    @jwt_required()
-    def get(self, user_id):
-        walks = Walk.query.filter_by(user_id=user_id).all()
-        return jsonify([walk.serialize() for walk in walks])
-
-    @jwt_required()
-    def post(self, user_id):
-        data = request.get_json()
-        new_walk = Walk(
-            location=data['location'],
-            distance=data['distance'],
-            photo=data['photo'],
-            user_id=user_id
-        )
-        db.session.add(new_walk)
+        walk = Walk(name=name, location=location, description=description, user_id=user_id)
+        db.session.add(walk)
         db.session.commit()
+
         return {"message": "Walk created successfully"}, 201
 
-class WalkSearch(Resource):
-    def get(self):
-        location = request.args.get('location')
-        walks = Walk.query.filter(Walk.location.ilike(f"%{location}%")).all()
-        return jsonify([walk.serialize() for walk in walks])
+api.add_resource(WalkResource, '/walks', '/walks/<int:walk_id>')
 
+class ReviewResource(Resource):
+    def get(self, review_id=None):
+        if review_id:
+            review = Review.query.get(review_id)
+            return review.to_dict() if review else {"error": "Review not found"}, 404
+        else:
+            reviews = Review.query.all()
+            return [review.to_dict() for review in reviews]
 
-class WalkReviews(Resource):
-    @jwt_required()
-    def get(self, walk_id):
-        walk = Walk.query.get_or_404(walk_id)
-        reviews = walk.reviews
-        return jsonify([review.to_dict() for review in reviews])
+    def post(self):
+        rating = request.json.get('rating')
+        text = request.json.get('text')
+        user_id = request.json.get('user_id')
+        walk_id = request.json.get('walk_id')
 
-    @jwt_required()
-    def post(self, walk_id):
-        data = request.get_json()
-        user_id = get_jwt_identity()
-        new_review = Review(
-            text=data['text'],
-            user_id=user_id,
-            walk_id=walk_id
-        )
-        db.session.add(new_review)
+        if not rating or not text or not user_id or not walk_id:
+            return {"error": "Rating, text, user_id, and walk_id are required fields"}, 400
+
+        review = Review(rating=rating, text=text, user_id=user_id, walk_id=walk_id)
+        db.session.add(review)
         db.session.commit()
+
         return {"message": "Review created successfully"}, 201
+api.add_resource(ReviewResource, '/reviews', '/reviews/<int:review_id>')
 
-class UserFollow(Resource):
-    @jwt_required()
-    def post(self, user_id):
-        follower_id = get_jwt_identity()
-        if user_id == follower_id:
-            return {"message": "You cannot follow yourself"}, 400
+class FollowResource(Resource):
+    def get(self, follow_id=None):
+        if follow_id:
+            follow = Follow.query.get(follow_id)
+            return follow.to_dict() if follow else {"error": "Follow not found"}, 404
+        follows = Follow.query.all()
+        return [follow.to_dict() for follow in follows]
 
-        follow = Follow.query.filter_by(follower_id=follower_id, followed_id=user_id).first()
-        if follow:
-            return {"message": "Already following"}, 400
+    def post(self):
+        follower_id = request.json.get('follower_id')
+        followed_id = request.json.get('followed_id')
 
-        new_follow = Follow(
-            follower_id=follower_id,
-            followed_id=user_id
-        )
-        db.session.add(new_follow)
+        if not follower_id or not followed_id:
+            return {"error": "Follower_id and followed_id are required fields"}, 400
+
+        follow = Follow(follower_id=follower_id, followed_id=followed_id)
+        db.session.add(follow)
         db.session.commit()
-        return {"message": "User followed successfully"}, 201
 
-    @jwt_required()
-    def delete(self, user_id):
-        follower_id = get_jwt_identity()
-        follow = Follow.query.filter_by(follower_id=follower_id, followed_id=user_id).first()
-        if not follow:
-            return {"message": "Not following"}, 400
+        return {"message": "Follow created successfully"}, 201
 
-        db.session.delete(follow)
-        db.session.commit()
-        return {"message": "User unfollowed successfully"}, 200
-
-api.add_resource(UserRegistration, '/register')
-api.add_resource(UserLogin, '/login')
-api.add_resource(UserWalks, '/user/<int:user_id>/walks')
-api.add_resource(WalkSearch, '/search')
-api.add_resource(WalkReviews, '/walk/<int:walk_id>/reviews')
-api.add_resource(UserFollow, '/user/<int:user_id>/follow')
+api.add_resource(FollowResource, '/follows', '/follows/<int:follow_id>')
 
 
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.json.get('username')
+    password = request.json.get('password')
 
+    if not username or not password:
+        return {"error": "Username and password are required"}, 400
+
+    user = User.query.filter_by(username=username).first()
+    if not user or not user.check_password(password):
+        return {"error": "Invalid credentials"}, 401
+
+    return {"message": "Logged in successfully"}, 200
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)    
+    app.run(port=5555, debug=True)
+
+
+
+
+
--- server/seed.py
+++ server/seed.py
@@ -1,9 +1,14 @@
-from datetime import datetime
-from app import app
-from models import db, User, Walk, Review, Follow
 
+
+from datetime import datetime
+from app import app, db, bcrypt
+from models import User, Walk, Review, Follow
 
 def seed_data():
+
+    db.drop_all()
+    db.create_all()
+
     # Create some users
     vanessa = User(username='vanessa', email='vanessa@email.com', password='password1')
     kim = User(username='kim', email='kim@email.com', password='password2')
@@ -12,28 +17,26 @@ def seed_data():
     db.session.add_all([vanessa, kim, kevin])
     db.session.commit()
 
-    #  walks for vanessa
-    vanessa_walk1 = Walk(location='Central Park', distance=2.5, photo='https://i.natgeofe.com/n/15ec8dec-df7c-45af-a0ae-08d4e906a134/belvedere-castle.jpg?w=2880&h=2160', user=vanessa, created_at=datetime(2022, 1, 1))
-    vanessa_walk2 = Walk(location='Hudson River Park', distance=3.2, photo='https://www.frommers.com/system/media_items/attachments/000/868/444/s980/Frommers-New-York-City-jogging-hudson-park-1190x768.jpg?1646914426', user=vanessa, created_at=datetime(2022, 2, 1))
+    # walks for vanessa
+    vanessa_walk1 = Walk(name='Central Park', distance=2.5, photo='https://i.natgeofe.com/n/15ec8dec-df7c-45af-a0ae-08d4e906a134/belvedere-castle.jpg?w=2880&h=2160', user_id=vanessa.id, )
+    vanessa_walk2 = Walk(name='Hudson River Park', distance=3.2, photo='https://www.frommers.com/system/media_items/attachments/000/868/444/s980/Frommers-New-York-City-jogging-hudson-park-1190x768.jpg?1646914426', user_id=vanessa.id,)
 
     db.session.add_all([vanessa_walk1, vanessa_walk2])
     db.session.commit()
 
     # reviews for Vanessa's walks
-    vanessa_review1 = Review(text='Great walk!', user=vanessa, walk=vanessa_walk1, created_at=datetime(2022, 1, 5))
-    vanessa_review2 = Review(text='Beautiful views!', user=vanessa, walk=vanessa_walk2, created_at=datetime(2022, 2, 5))
+    vanessa_review1 = Review(text='Great walk!', user_id=vanessa.id, walk_id=vanessa_walk1.id, rating=5, )
+    vanessa_review2 = Review(text='Beautiful views!', user_id=vanessa.id, walk_id=vanessa_walk2.id, rating=4,)
 
     db.session.add_all([vanessa_review1, vanessa_review2])
     db.session.commit()
 
     # Have kim follow vanessa
-    kim_follow_vanessa = Follow(follower=kim, followed=vanessa, created_at=datetime(2022, 3, 1))
+    kim_follow_vanessa = Follow(follower_id=kim.id, followed_id=vanessa.id,)
     db.session.add(kim_follow_vanessa)
     db.session.commit()
+
 
 if __name__ == '__main__':
     with app.app_context():
         seed_data()
-
-
-
