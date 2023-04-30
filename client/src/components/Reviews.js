65c778678939de0c6e10ed1907e311df03ce4cda
vanessa coelho <vanessa.co403@gmail.com>
2023-04-30 01:06:53 -0400

    review changes

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -1,12 +1,22 @@
 
 
 import React, { useState } from 'react';
-import { Box, Typography, TextField, Button, Rating, Paper } from '@mui/material';
+import {
+  Box,
+  Typography,
+  TextField,
+  Button,
+  Rating,
+  Paper,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from '@mui/material';
 import { styled } from '@mui/system';
 import { useReviews } from '../contexts/ReviewsContext';
 import OpenStreetMapLocation from './OpenStreetMapLocation';
 import 'leaflet/dist/leaflet.css';
-
 
 const Container = styled(Box)(({ theme }) => ({
   display: 'flex',
@@ -43,24 +53,24 @@ const PaperStyled = styled(Paper)(({ theme }) => ({
   maxWidth: '600px',
 }));
 
-
 function Reviews() {
   const { reviews, addReview, editReview, deleteReview } = useReviews();
   const [reviewText, setReviewText] = useState('');
   const [reviewRating, setReviewRating] = useState('');
   const [reviewLocation, setReviewLocation] = useState('');
   const [editReviewId, setEditReviewId] = useState(null);
+  const [openDialog, setOpenDialog] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     const newReview = {
       text: reviewText,
       rating: reviewRating,
-      user_id: 1,
+      user_id: 2,
       location: reviewLocation,
     };
     try {
-      const addedReview = await addReview(newReview);
+      await addReview(newReview);
       setReviewText('');
       setReviewRating('');
       setReviewLocation('');
@@ -69,11 +79,20 @@ function Reviews() {
     }
   };
 
-  const handleEdit = (id, text, rating, comment) => {
+  const handleEdit = (id, text, rating, location) => {
     setEditReviewId(id);
     setReviewText(text);
     setReviewRating(rating);
-    setReviewLocation(comment);
+    setReviewLocation(location);
+    setOpenDialog(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+    setEditReviewId(null);
+    setReviewText('');
+    setReviewRating('');
+    setReviewLocation('');
   };
 
   const handleSaveEdit = async (event) => {
@@ -82,11 +101,12 @@ function Reviews() {
       id: editReviewId,
       text: reviewText,
       rating: reviewRating,
-      user_id: 1, 
+      user_id: 2,
       location: reviewLocation,
     };
     try {
-      const editedReview = await editReview(editReviewId, updatedReview);
+      await editReview(editReviewId, updatedReview);
+      setOpenDialog(false);
       setEditReviewId(null);
       setReviewText('');
       setReviewRating('');
@@ -136,32 +156,28 @@ function Reviews() {
         </SubmitButton>
       </Form>
       <Typography variant="h5" gutterBottom>
-        {/* All Reviews */}
+        All Reviews
       </Typography>
       {reviews.map((review) => (
-  <PaperStyled key={review.id}>
-    <Typography variant="h6">Review by User {review.user_id}</Typography>
-    <Rating value={review.rating} readOnly />
-    <Typography>{review.text}</Typography>
-    {review.location && review.location.lat && review.location.lng && (
-      <>
-        <Typography>
-          Location: {review.location.lat}, {review.location.lng}
-        </Typography>
-        <OpenStreetMapLocation location={review.location} />
-      </>
-         )}
-          {editReviewId !== review.id ? (
-            <Button
-              variant="outlined"
-              color="primary"
-              onClick={() => handleEdit(review.id, review.text, review.rating, review.location)}
-            >
-              Edit Review
-            </Button>
-          ) : (
-            <></>
+        <PaperStyled key={review.id}>
+          <Typography variant="h6">Review by User {review.user_id}</Typography>
+          <Rating value={review.rating} readOnly />
+          <Typography>{review.text}</Typography>
+          {review.location && review.location.lat && review.location.lng && (
+            <>
+              <Typography>
+                Location: {review.location.lat}, {review.location.lng}
+              </Typography>
+              <OpenStreetMapLocation location={review.location} />
+            </>
           )}
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={() => handleEdit(review.id, review.text, review.rating, review.location)}
+          >
+            Edit Review
+          </Button>
           <Button
             variant="outlined"
             color="secondary"
@@ -173,51 +189,51 @@ function Reviews() {
         </PaperStyled>
       ))}
       {editReviewId !== null && (
-        <Form onSubmit={handleSaveEdit}>
-          <TextFieldStyled
-            label="Edit Review"
-            multiline
-            rows={6}
-            value={reviewText}
-            onChange={(event) => setReviewText(event.target.value)}
-          />
-          <RatingStyled>
-            <Typography>Rating:</Typography>
-            <Rating
-              value={reviewRating}
-              onChange={(event, newValue) => setReviewRating(newValue)}
-              size="large"
-              precision={1}
-            />
-          </RatingStyled>
-          <TextFieldStyled
-            label="Location"
-            value={reviewLocation}
-            onChange={(event) => setReviewLocation(event.target.value)}
-          />
-          <Box>
-            <SubmitButton type="submit" variant="contained" color="primary">
-              Save Edit
-            </SubmitButton>
-            <Button
-              variant="outlined"
-              onClick={() => {
-                setEditReviewId(null);
-                setReviewText('');
-                setReviewRating('');
-                setReviewLocation('');
-              }}
-              style={{ marginLeft: '1rem' }}
-            >
-              Cancel Edit
-            </Button>
-          </Box>
-        </Form>
+        <Dialog open={openDialog} onClose={handleCloseDialog} fullWidth maxWidth="sm">
+          <DialogTitle>Edit Review</DialogTitle>
+          <DialogContent>
+            <Form onSubmit={handleSaveEdit}>
+              <TextFieldStyled
+                label="Edit Review"
+                multiline
+                rows={6}
+                value={reviewText}
+                onChange={(event) => setReviewText(event.target.value)}
+              />
+              <RatingStyled>
+                <Typography>Rating:</Typography>
+                <Rating
+                  value={reviewRating}
+                  onChange={(event, newValue) => setReviewRating(newValue)}
+                  size="large"
+                  precision={1}
+                />
+              </RatingStyled>
+              <TextFieldStyled
+                label="Location"
+                value={reviewLocation}
+                onChange={(event) => setReviewLocation(event.target.value)}
+              />
+              <DialogActions>
+                <SubmitButton type="submit" variant="contained" color="primary">
+                  Save Edit
+                </SubmitButton>
+                <Button
+                  variant="outlined"
+                  onClick={handleCloseDialog}
+                  style={{ marginLeft: '1rem' }}
+                >
+                  Cancel Edit
+                </Button>
+              </DialogActions>
+            </Form>
+          </DialogContent>
+        </Dialog>
       )}
     </Container>
   );
-}; 
-  
+}
 
 export default Reviews;
+
 
--- client/src/components/Walks.js
+++ client/src/components/Walks.js
@@ -1,3 +1,42 @@
+// import React, { useState, useEffect, useContext } from 'react';
+// import { AuthContext } from '../contexts/AuthContext';
+
+// function Walks() {
+//   const [walks, setWalks] = useState([]);
+//   const { user } = useContext(AuthContext);
+
+//   useEffect(() => {
+//     const fetchWalks = async () => {
+//       const response = await fetch('/walks');
+//       const data = await response.json();
+//       setWalks(data);
+//     };
+
+//     fetchWalks();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h2>Walks</h2>
+//       {user && <p>Welcome, {user.username}!</p>}
+//       <ul>
+//         {walks.map((walk) => (
+//           <li key={walk.id}>
+//             <h3>{walk.location}</h3>
+//             <div>Description: {walk.description}</div>
+//             <div>
+//               <img src={walk.photo} alt={walk.name} style={{ width: '300px', height: '200px' }} />
+//             </div>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default Walks;
+
+
 import React, { useState, useEffect, useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 
@@ -22,11 +61,16 @@ function Walks() {
       <ul>
         {walks.map((walk) => (
           <li key={walk.id}>
-            <h3>{walk.location}</h3>
-            <div>Description: {walk.description}</div>
-            <div>
-              <img src={walk.photo} alt={walk.name} style={{ width: '300px', height: '200px' }} />
-            </div>
+            <h3>{walk.name}</h3>
+            <div>Location: {walk.location}</div>
+            <div>Distance: {walk.distance} km</div>
+            {walk.photo && (
+              <div>
+                <img src={walk.photo} alt={walk.name} style={{ width: '300px', height: '200px' }} />
+              </div>
+            )}
+            {walk.description && <div>Description: {walk.description}</div>}
+            <div>Added by: {walk.username}</div>
           </li>
         ))}
       </ul>
